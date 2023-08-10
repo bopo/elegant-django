f97cbba6d5f16e1150b1cfdc4813cd086de25905
bopo <ibopo@126.com>
2023-08-10 13:32:40 +0800

    fix: date icon

--- example/example/static/admin/js/jquery.init.js
+++
@@ -1,8 +0,0 @@
-var django = django || {};
-django.jQuery = jQuery.noConflict(true);
-
-// Override Django jquery.init.js
-// Make jQuery global - needed for Django-Select2
-if (!window.jQuery)
-    window.$ = window.jQuery = django.jQuery;
-
