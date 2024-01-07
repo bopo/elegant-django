cb47847f6a3d2103f38a66c57b48209a78b97304
bopo <ibopo@126.com>
2024-01-07 17:51:26 +0800

    bump: version 0.1.0 → 0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,16 @@
+## v0.1.1 (2024-01-07)
+
+### Fix
+
+- tag list
+- filer css
+- clean poetry
+- clean log
+- clean log
+- clean log
+- update test
+- update test
+- SuitSplitDateTimeWidget
+- filter css path
+- date icon
+- date icon
--- elegant/__init__.py
+++ elegant/__init__.py
@@ -4,5 +4,5 @@ from .mixins.objects import BaseDjangoObjectActions
 from .mixins.objects import DjangoObjectActions
 from .mixins.objects import takes_instance_or_queryset
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 VERSION = __version__
